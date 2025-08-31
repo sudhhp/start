51139f44e35717e8531e7f9a8ca4b074552c4f98
sudhhp <soodi.hp@gmail.com>
2025-08-31 16:12:13 +0330

    ratelimited extended

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const ratelimiter = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(8, "20 s"),
+  limiter: Ratelimit.slidingWindow(100, "60 s"),
 });
 
 export default ratelimiter;
