54511d1a803c66ffdc3fb7462c7a7d06eece26a8
sudhhp <soodi.hp@gmail.com>
2025-08-31 14:27:05 +0330

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,14 @@ import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 // Load environment variables
 dotenv.config();
 
 // Connect to the database
 
 const app = express();
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 // Middleware to use the router for "/api/notes"
 app.use(express.json()); //to be access to req.body parameter
@@ -17,13 +19,24 @@ app.use((req, res, next) => {
   console.log(`req method is ${req.method} and req URL is ${req.url}`);
   next();
 });
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
 app.use(rateLimiter);
 app.use("/api/notes", noteRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+}
+
 connectDB().then(() => {
   // Start the server
   app.listen(PORT, () => {
